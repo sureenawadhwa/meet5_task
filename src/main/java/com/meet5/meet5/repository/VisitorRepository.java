c08f44900bc208f47a9a7b0af39748e7de7ad7fb
sureenawadhwa <sureenawadhwa487@gmail.com>
2022-12-15 01:26:43 +0530

    used configurable fraudulent threshold

--- src/main/java/com/meet5/meet5/repository/VisitorRepository.java
+++ src/main/java/com/meet5/meet5/repository/VisitorRepository.java
@@ -2,6 +2,7 @@ package com.meet5.meet5.repository;
 
 import com.meet5.meet5.entity.Meet5Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +14,9 @@ public class VisitorRepository {
 
     @Autowired private JdbcTemplate jdbcTemplate;
 
+    @Value("${fraudulentThreshold:100}")
+    private int fraudulentThreshold;
+
     public Meet5Visitor save(Meet5Visitor meet5Visitor) {
         jdbcTemplate.update("INSERT into meet5_visitor VALUES (?, ?, ?)",
                 meet5Visitor.getVisitor_id(), meet5Visitor.getVisited_id(), meet5Visitor.getVisited_ts());
@@ -20,9 +24,9 @@ public class VisitorRepository {
     }
 
     public boolean isFraudulent(String id){
-        String query = "SELECT count(*) >=2 from meet5_user " +
+        String query = "SELECT count(*) >=? from meet5_user " +
                 "INNER JOIN meet5_visitor ON meet5_user.id = meet5_visitor.visitor_id " +
                 "where TIMESTAMPDIFF(MINUTE, created_ts, visited_ts) < 10 and visitor_id = ?";
-        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, boolean.class, id));
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, boolean.class,fraudulentThreshold,id));
     }
 }
